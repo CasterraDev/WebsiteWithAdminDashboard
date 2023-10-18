fd4004126fd645da2a40d04168ee2cdd73234fae
CasterraDev <casterradev@gmail.com>
2023-10-18 11:48:14 -0400

    Services responsiveness

--- components/Services.jsx
+++ components/Services.jsx
@@ -4,7 +4,7 @@ import Service from "./Service";
 export default function Services(){
     return(
         <div className={styles.wrapper}>
-            <h1>Services</h1>
+            <h1 className={styles.title}>Services</h1>
             <div className={styles.servicesWrapper}>
                 <Service width="250px" />
                 <Service width="250px" />
